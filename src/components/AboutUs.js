3124dd637553b54befe6530f4c14006f9659ac13
Alexander Khvoshchev <shsha.khv@gmail.com>
2024-09-05 01:36:53 +0300

    about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,7 @@
 const AboutUs = () => {
   return (
-    <div>
-      <div>
+    <div className="aboutUs">
+      <div className="aboutUsLeft">
         <div>Коротко о нас</div>
         <div>Более 9 лет на рынке</div>
         <div>
@@ -16,14 +16,26 @@ const AboutUs = () => {
         <div>Работаем как с юридическими, так и с физическими лицами</div>
         <div>Не накручиваем цены и не навязываем ненужные допуслуги</div>
       </div>
-      <div>
-        <img src="/Building1.svg" alt="Building 1" />
-        <img src="/Building2.svg" alt="Building 2" />
-        <div className="smallBlocks">
-          <div>{">"}500 обследовано зданий и сооружений</div>
-          <div>{">"}600 квартир прошли проверку на качество ремонта</div>
+
+      <div className="aboutUsRight">
+        <div className="leftHalf">
+          <img src="/Building1.svg" alt="Building 1" className="leftHalfImg" />
+          <div className="leftHalfSmallBlocks">
+            <div className="smallBlock1">
+              <div className="smallBlock1Text">{">"}500</div> обследовано зданий и сооружений
+            </div>
+            <div className="smallBlock1">
+              <div className="smallBlock1Text">{">"}600 </div>квартир прошли проверку на качество ремонта
+            </div>
+          </div>
         </div>
-        <div>{">"}2500 квартирам мы оказали помощь при затоплении</div>
+
+        <div className="rigthHalf">
+          <img src="/Building2.svg" alt="Building 2" />
+          <div className="smallBlock2">
+            <div className="smallBlock1Text">{">"}2500</div> квартирам мы оказали помощь при затоплении
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <> </>;
+  return <> Footer </>;
 };
 
 export default Footer;
